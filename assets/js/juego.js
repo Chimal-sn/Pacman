2bd90bc684e19f025f80290e49d9ea8db8addc5f
Sony <sonychimo10@hotmail.com>
2026-06-28 15:56:53 -0600

    Update. Mapa dibujado con la matriz creada

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,3 +8,47 @@ const mapa = [
 const canvas = document.getElementById('canvas-juego');
 const ctx = canvas.getContext('2d');
 const tamañoCelda = 24;
+const numeroFilas = mapa.length;
+const numeroColumnas = mapa[0].length;
+
+canvas.width = numeroColumnas * tamañoCelda;
+canvas.height = numeroFilas * tamañoCelda;
+
+let blink = true;
+
+function dibujarMapa() {
+    for (let fila = 0; fila < numeroFilas; fila++) {
+        for (let columna = 0; columna < numeroColumnas; columna++) {
+            const tipo = mapa[fila][columna];
+            let x = columna * tamañoCelda;
+            let y = fila * tamañoCelda;
+            const centroX = x + tamañoCelda / 2;
+            const centroY = y + tamañoCelda / 2;
+            const anguloInicio = 0;
+            const anguloFin = Math.PI * 2;
+
+            if (tipo == 1) {
+                ctx.fillStyle = '#0e0e96';
+                ctx.fillRect(x, y, tamañoCelda, tamañoCelda);
+            } else if (tipo == 0) {
+                ctx.beginPath();
+                const radio = 2.5;
+                ctx.arc(centroX, centroY, radio, anguloInicio, anguloFin);
+                ctx.fillStyle = '#ffb8ae';
+                ctx.fill();
+                ctx.closePath();
+            } else if (tipo == 2) {
+                if (blink) {
+                    ctx.beginPath();
+                    const radio = 4;
+                    ctx.arc(centroX, centroY, radio, anguloInicio, anguloFin);
+                    ctx.fillStyle = '#ffb8ae';
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+        }
+    }
+}
+
+dibujarMapa();

1cab578cb4804b8b08d0c84324a33122bc11e6ca
Sony <sonychimo10@hotmail.com>
2026-06-28 12:58:51 -0600

    Update. Matriz agregado junto con el script y html para el juego en funcionamiento

---
+++ assets/js/juego.js
@@ -0,0 +1,10 @@
+const mapa = [
+    [1, 1, 1, 1, 1],
+    [1, 2, 0, 2, 1],
+    [1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1]
+];
+
+const canvas = document.getElementById('canvas-juego');
+const ctx = canvas.getContext('2d');
+const tamañoCelda = 24;

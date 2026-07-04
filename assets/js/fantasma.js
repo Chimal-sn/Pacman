a061bc9d52303947a252d6812b74f6b3888e3d05
Sony <sonychimo10@hotmail.com>
2026-07-03 21:47:16 -0600

    Fix. Evitar sobresaturacion en los controles para movil

---
+++ assets/js/fantasma.js
@@ -0,0 +1,15 @@
+export const fantasma = {
+
+    posicionX: 9,
+    posicionY: 8,
+    velocidad: 0.05,
+    direccionActual: '',
+    direccionSiguiente: '',
+    angulo: 0,
+
+    colores: ['#ff0000', '#00ffff', '#ffcc00', '#9400d3'],
+
+    dibujar: function (ctx, tamañoCelda, colores) {
+
+    }
+}
